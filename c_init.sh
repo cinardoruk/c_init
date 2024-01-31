cbdab418b64f38bb17c5c78bd3ea60f57af65241
Cinar Doruk <cinar.doruk@gmail.com>
2024-01-31 09:46:39 +0300

    works. fixed paths and makefile edits

--- c_init.sh
+++ c_init.sh
@@ -1,36 +1,34 @@
 #!/bin/zsh
 
 # Check if at least one argument is provided
+
 if [ $# -ne 1 ]; then
     echo "Usage: $0 project_name"
     echo "Creates directory project_name w/ project_name.c and makefile in it"
     exit 1
 fi
 
-#create new directory, cd into it
+#create new directory,
 mkdir $1
-cd $1
 
 #create an empty .c file from template
-c_template="/~/Scripts/template.c"
-cp $c_template ./$1.c
+c_template="./template.c"
+c_new="./$1/$1.c"
+cp $c_template $c_new
 
 #create a makefile for it from template
-mf_template="~/Scripts/makefile_template"
-cp $mf_template ./makefile
+mf_template="./makefile_template"
+mf_new="./$1/makefile"
+cp $mf_template $mf_new
 
 #modify new makefile so it works with $1.c
 
-makefile_path = "./makefile"
-
-#change the name of the executable target to $1
-sed -i "/^EXECUTABLE = / s/myprogram/$1/"
-
 # Check if the Makefile exists
-if [ -f "$makefile_path" ]; then
+if [ -f "$mf_new" ]; then
     # Use sed to replace the SOURCE line in the Makefile
-    sed -i "s/SOURCE = .*/SOURCE = $1/" "$makefile_path"
-    echo "Changed SOURCE to $1 in $makefile_path"
+    sed -i "s/SOURCE = .*/SOURCE = $1.c/" $mf_new
+    sed -i "s/EXECUTABLE = .*/EXECUTABLE = $1/" $mf_new
+
 else
-    echo "Makefile not found at $makefile_path"
+    echo "Makefile not found at $mf_new"
 fi
